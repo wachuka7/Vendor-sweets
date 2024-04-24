43a83f9b6f1752056272a9d45605228759b80ce3
wachuka7 <rachaelwachuka7@gmail.com>
2024-04-24 18:14:40 +0300

    feat: put the routes in a routes.py file

--- server/debug.py
+++ server/debug.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 from app import app
-from models import db, Sweet, Vendor, VendorSweet
+from models import db
+from models.sweet import Sweet
+from models.vendor import Vendor
+from models.vendor_sweet import  VendorSweet
 
 if __name__ == '__main__':
     with app.app_context():
